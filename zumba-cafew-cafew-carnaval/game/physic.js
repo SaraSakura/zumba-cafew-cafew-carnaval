9f393e325c81def2f2d336f1d44d7b4844a781e2
SaraSakura <50243116+SaraSakura@users.noreply.github.com>
2020-09-30 22:08:44 +0200

    Add files via upload

---
+++ zumba-cafew-cafew-carnaval/game/physic.js
@@ -0,0 +1,28 @@
+function update()
+{
+    var delta = clock.getDelta(); // seconds.
+    var moveDistance = 50 * delta; // 200 pixels per second
+    var rotateAngle = Math.PI / 2 * delta * 2;   // pi/2 radians (90 degrees) per second
+
+    if (keyboard.pressed("left"))
+        player1.turnLeft(rotateAngle);
+    if (keyboard.pressed("right"))
+        player1.turnRight(rotateAngle);
+    if (keyboard.pressed("up"))
+        player1.accelerate(moveDistance);
+    if (keyboard.pressed("down"))
+        player1.decelerate(moveDistance);
+
+
+
+    if (sec < 50)
+        ennemy.accelerate(moveDistance);
+    if (sec > 100)
+        ennemy.decelerate(moveDistance);
+    if (sec == 150)
+        sec = 0;
+    sec++;
+    player1.move();
+    ennemy.move();
+    controls.update();
+}
---
+++ zumba-cafew-cafew-carnaval/game/shoot.js
@@ -0,0 +1,116 @@
+var bulletTime1 = 0;
+
+var bullet_player1_material = new THREE.MeshLambertMaterial(
+{
+    color: 0x00ff00, 
+    transparent: false
+});
+
+function shoot()
+{
+    if (keyboard.pressed("space") && bulletTime1 + 0.8 < clock.getElapsedTime())
+    {
+        bullet = new THREE.Mesh(
+            new THREE.SphereGeometry(2),
+            bullet_player1_material);
+        scene.add(bullet);
+        bullet.position.x = player1.graphic.position.x + 7.5 * Math.cos(player1.direction);
+        bullet.position.y = player1.graphic.position.y + 7.5 * Math.sin(player1.direction);
+        bullet.angle = player1.direction;
+        player1.bullets.push(bullet);
+        bulletTime1 = clock.getElapsedTime();
+    } 
+
+    // move bullets
+    var moveDistance = 5;
+    for (var i = 0; i < player1.bullets.length; i++)
+    {
+        console.log("lala");
+            player1.bullets[i].position.x += moveDistance * Math.cos(player1.bullets[i].angle);
+            player1.bullets[i].position.y += moveDistance * Math.sin(player1.bullets[i].angle);
+    }
+
+}
+
+function collisions()
+{
+    bullet_collision();
+    player_collision();
+    player_falling();
+}
+
+function bullet_collision()
+{
+    //collision between bullet and walls
+    for (var i = 0; i < player1.bullets.length; i++)
+    {
+        if (Math.abs(player1.bullets[i].position.x) >= WIDTH / 2 ||
+            Math.abs(player1.bullets[i].position.y) >= HEIGHT / 2)
+        {
+            scene.remove(player1.bullets[i]);
+            player1.bullets.splice(i, 1);
+            i--;
+        }
+    }
+
+}
+
+function player_collision()
+{
+    //collision between player and walls
+    var x = player1.graphic.position.x + WIDTH / 2;
+    var y = player1.graphic.position.y + HEIGHT / 2;
+    if ( x < 0 )
+        player1.graphic.position.x -= x;
+    if ( x > WIDTH )
+        player1.graphic.position.x -= x - WIDTH;
+    if ( y < 0 )
+        player1.graphic.position.y -= y;
+    if ( y > HEIGHT )
+        player1.graphic.position.y -= y - HEIGHT;
+
+    var x2 = ennemy.graphic.position.x + WIDTH / 2;
+    var y2 = ennemy.graphic.position.y + HEIGHT / 2;
+    if ( x2 < 0 )
+        ennemy.graphic.position.x -= x2;
+    if ( x2 > WIDTH )
+        ennemy.graphic.position.x -= x2 - WIDTH;
+    if ( y2 < 0 )
+        ennemy.graphic.position.y -= y2;
+    if ( y > HEIGHT )
+        ennemy.graphic.position.y -= y2 - HEIGHT;
+}
+
+
+function player_falling()
+{
+    var nb_tile = 10;
+    var sizeOfTileX = WIDTH / nb_tile;
+    var sizeOfTileY = HEIGHT / nb_tile;
+    var x = player1.graphic.position.x | 0;
+    var y = player1.graphic.position.y | 0;
+    var length = noGround.length;
+    var element = null;
+
+    for (let i = 0; i < length; i++) {
+        element = noGround[i];
+        if (element == null)
+            continue;
+        let tileX = (element[0]) | 0;
+        let tileY = (element[1]) | 0;
+        let mtileX = (element[0] + sizeOfTileX) | 0;
+        let mtileY = (element[1] + sizeOfTileY) | 0;
+
+        if ((x > tileX)
+            && (x < mtileX)
+            && (y > tileY) 
+            && (y < mtileY))
+        {
+            if (player1.life > 0)
+                player1.life -= 1;
+            else
+                player1.dead();
+        }
+    }
+
+}
